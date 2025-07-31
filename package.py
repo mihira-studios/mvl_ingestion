6c2fb60c470d638e0a434dede027b235cb7e9924
Deepak Thapliyal <deepakt@mvlhq.mihiralabs.com>
2025-07-31 14:21:35 +0530

    bugfix: update package runtime dependencies

--- package.py
+++ package.py
@@ -5,11 +5,14 @@ version = "0.2.1"
 requires = [
     "~python-3",
     "PyYAML-6",
+    "coloredlogs",
+    "OpenImageIO",
+    "mvl_core_pipeline-0",
+    "mvl_make_dailies-0"
 ]
 
 private_build_requires = [
     "python-3",
-    "mvl_core_pipeline",
     "mvl_rez_package_builder",
 ]
 
